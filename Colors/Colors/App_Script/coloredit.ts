7ac45fa24c15979f6bf8e6b66d77799423f2002e
Bill Wagner <bill.w.wagner@outlook.com>
2014-05-29 19:14:10 -0400

    create the edit color control

---
+++ Colors/Colors/App_Script/coloredit.ts
@@ -0,0 +1,22 @@
+﻿module Colors.Controls {
+    class ColorEditController {
+        constructor($scope: ng.IScope, $element: HTMLElement) {
+        }
+    }
+
+    export function ColorEditControl(): ng.IDirective {
+        var directive: ng.IDirective = {};
+        directive.restrict = "E";
+        directive.transclude = true;
+        directive.scope = {
+            editColor: "=target"
+        };
+        directive.template = "<span>" +
+        "<input ng-style=\"{ color:editColor.value }\"  type=\"text\" ng-model=\"editColor.name\" />" +
+        "<input type=\"text\" ng-model=\"editColor.value\" />" +
+        "</span>";
+        directive.replace = true;
+        directive.controller = ColorEditController;
+        return directive;
+    }
+} 
