eafbb0d893e9da4f8cb497115500f3dc71226798
Bill Wagner <bill.w.wagner@outlook.com>
2014-05-29 18:08:43 -0400

    create data service

---
+++ Colors/Colors/App_Script/colorDataService.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular-resource.d.ts" />
+
+module Colors {
+
+    export interface IColor {
+        Id: number;
+        name: string;
+        value: string;
+    }
+
+    export class ColorDataService {
+        constructor($resource: ng.resource.IResourceService) {
+            this.resource = $resource("api/Colors/:id",
+                { id: "@id" },
+                {
+                    query: { method: "GET", isArray: true },
+                });
+        }
+
+        public getAllColors() {
+            return this.resource.query();
+        }
+
+        private resource: ng.resource.IResourceClass<ng.resource.IResource<IColor>>;
+    }
+
+}
