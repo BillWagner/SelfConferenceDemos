ca93bdc2714fd499c07e672f6f58a7aa7886c5ad
Bill Wagner <bill.w.wagner@outlook.com>
2014-05-29 18:20:52 -0400

    created the controller
    Controller now exists

---
+++ Colors/Colors/App_Script/pageController.ts
@@ -0,0 +1,13 @@
+﻿
+
+module Colors {
+    export class PageController {
+        constructor(
+            private $scope: ng.IScope,
+            private colorDataService: ColorDataService) {
+            this.colorData = colorDataService.getAllColors();
+        }
+
+        colorData: ng.resource.IResourceArray<ng.resource.IResource<IColor>>;
+    }
+}
